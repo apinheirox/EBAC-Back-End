48e50e878751b3d75113661ecbf27b20e51a2fc9
Antônio Pinheiro <techwoodev@gmail.com>
2024-04-29 11:53:49 -0300

    .

---
+++ TarefaCalculoMedia/src/media.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+
+    public static void main(String[] args) {
+        // Criando um objeto Scanner para ler as entradas do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando e lendo as notas
+        System.out.println("Digite a primeira nota:");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota:");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota:");
+        double nota3 = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota:");
+        double nota4 = scanner.nextDouble();
+
+        // Calculando a média
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        // Exibindo o resultado
+        System.out.println("A média das notas é: " + media);
+
+        // Fechando o scanner
+        scanner.close();
+    }
+}
+
