3de6edfffe6ad7044657771eff1f4d78469e2a18
Antônio Pinheiro <164817129+apinheirox@users.noreply.github.com>
2024-05-01 11:40:21 -0300

    Update annotation.java

--- TarefaAnnotation/src/annotation.java
+++ TarefaAnnotation/src/annotation.java
@@ -1,11 +1,28 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 // Definição da annotation Tabela
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface Tabela {
+@interface Tabela {
     String nome();
+}
+
+// Classe exemplo que utiliza a annotation Tabela
+@Tabela(nome = "clientes")
+class Cliente {
+    // Métodos e propriedades da classe Cliente
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Acessando a annotation na classe Cliente
+        Tabela annotation = Cliente.class.getAnnotation(Tabela.class);
+
+        if (annotation != null) {
+            String nomeTabela = annotation.nome();
+            System.out.println("O nome da tabela da classe Cliente é: " + nomeTabela);
+        } else {
+            System.out.println("A classe Cliente não possui a annotation Tabela.");
+        }
+    }
 }
